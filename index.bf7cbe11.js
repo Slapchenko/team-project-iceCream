fadceda9c75047f96b371c3f87fd8f531bdd16c9
Slapchenko <103333806+Slapchenko@users.noreply.github.com>
2022-06-29 09:41:30 +0000

    Deploying to gh-pages from @ Slapchenko/team-project-iceCream@355b44db5edf6e01da5594ca4a333d8b6f81540f 🚀

--- index.bf7cbe11.js
+++
@@ -1,2 +0,0 @@
-function readMore(){var e=document.getElementById("dots"),n=document.getElementById("more"),t=document.getElementById("btn");"none"===e.style.display?(e.style.display="inline",t.innerHTML="Read more",n.style.display="none"):(e.style.display="none",t.innerHTML="Hide",n.style.display="inline")}
-//# sourceMappingURL=index.bf7cbe11.js.map
