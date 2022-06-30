1c33677a6e952d90b16a5fa0dadf0bd211b3b5d6
Slapchenko <103333806+Slapchenko@users.noreply.github.com>
2022-06-30 07:29:19 +0000

    Deploying to gh-pages from @ Slapchenko/team-project-iceCream@9a3b3562e3f49f78b2eea96ad22f71922a18c98a 🚀

--- index.cf31140f.js
+++
@@ -1,2 +0,0 @@
-(()=>{const e={openModalBtn:document.querySelector("[data-modal-open3]"),closeModalBtn:document.querySelector("[data-modal-close3]"),modal:document.querySelector("[data-modal3]")};function o(){e.modal.classList.toggle("is-hidden3")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)})();
-//# sourceMappingURL=index.cf31140f.js.map
